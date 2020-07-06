dc4529c5a5e844f9fcd19b0b4a92ab5b1529eec8
dreamerryao <244791255@qq.com>
2020-07-07 01:27:39 +0800

    add build dic

---
+++ build/precache-manifest.b18786ecac6572e725465e282ceac10b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02975310d57501bb456c588cd1412df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed2857809a5c8096eef5",
+    "url": "/static/js/2.18f8378b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.18f8378b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1304341f1e7bdc82faa6",
+    "url": "/static/js/main.0e60edfd.chunk.js"
+  },
+  {
+    "revision": "21b7a31307c2b37318a8",
+    "url": "/static/js/runtime-main.bf897520.js"
+  }
+]);
