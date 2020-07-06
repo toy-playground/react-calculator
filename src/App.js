728356e5ecd613dedbb287ebbf8b17863945833f
dreamerryao <244791255@qq.com>
2020-07-07 01:22:55 +0800

    feat: create sci mode

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import { green } from '@material-ui/core/colors';
-import ListItem from '@material-ui/core/ListItem';
-import Paper from '@material-ui/core/Paper';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+
 import More from './More'
 import CommonCal from './CommonCal'
+import SciCal from './sciCal'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link as RouterLink,
-  useRouteMatch,
-  useParams
 } from "react-router-dom";
 
 
@@ -69,10 +61,10 @@ export default function App() {
               Calculator
           </Typography>
             <Button color="inherit" component={RouterLink} size="large" to="/">
-              Mode1
+              普通
             </Button>
             <Button color="inherit" component={RouterLink} size="large" to="/mode">
-              Mode2
+              科学
             </Button>
             <Button color="inherit" component={RouterLink} size="large"  to="/more">
               More
@@ -84,8 +76,8 @@ export default function App() {
             <More />
           </Route>
           <Route path="/mode">
-            <h2>科学计算模式</h2>
-            {/* <Mode /> */}
+            <SciCal></SciCal>
+            {/* <h2>科学计算模式</h2> */}
           </Route>
           <Route path="/">
           {/* <h2>普通计算模式</h2> */}
