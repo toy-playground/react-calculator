51e803167da54ba84c22a37872604bac7d39773b
wangzuobin <wangzuobin@bytedance.com>
2021-07-17 15:16:25 +0800

    gh-pages

---
+++ build/precache-manifest.238bd56588fc24bdfa556174c38aaff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c0119710d4c720912d2163d447c0d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "417b5c1b7f55671a2f05",
+    "url": "/static/js/2.c4768d11.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c4768d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a787e13e89669bd61d",
+    "url": "/static/js/main.50ccb7ce.chunk.js"
+  },
+  {
+    "revision": "21b7a31307c2b37318a8",
+    "url": "/static/js/runtime-main.bf897520.js"
+  }
+]);
--- src/context/resContext.js
+++
@@ -1,4 +0,0 @@
-import React  from 'react';
-
-export const resContext = React.createContext();
-export default resContext;
